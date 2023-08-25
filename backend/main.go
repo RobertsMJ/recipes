12452975101d3dafdc22e52ad86b781c18b7b7b2
mitch <robertsmj1138@gmail.com>
2023-08-25 14:20:53 -0400

    more dev env experimentation

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Recipe struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -14,11 +19,20 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, "pong")
 	})
 
+	r.GET("/recipes", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, []Recipe{
+			{
+				Name:        "Food 1",
+				Description: "I am a food, 1",
+			},
+		})
+	})
+
 	return r
 }
 
 func main() {
 	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
+	// Listen and Serve on localhost:8080
 	r.Run(":8080")
 }
